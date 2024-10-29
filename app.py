527d6710f2d2e953302f32383fdde4affa18937a
lukefisha <lafbbz@umsystem.edu>
2024-10-29 13:31:21 -0500

    Added create page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
+app.config['SECRET_KEY'] = 'your secret key'
 
 
 # Function to open a connection to the database.db file
@@ -34,6 +34,28 @@ def index():
 
 
 # route to create a post
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    #determine if the page is being requested with a POST or GET request
+    if request.method == 'POST':
+        #get the title and content that was submitted
+        title = request.form['title']
+        content = request.form['content']
 
+        #display an error if the title or content is not submitted
+        #else make a db connection and insert the blog post content
+        if not title:
+            flash("Title is required")
+        elif not content:
+            flash("Content is required")
+        else:
+            conn = get_db_connection()
+            #insert into db
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
 
 app.run(port=5008)
